b7db4bba91954222ea1ca2714146bcc490d0ef04
Camila Barbosa <camilagomss@gmail.com>
2019-03-04 19:24:56 -0300

    week 2 assignment complete

--- 103/image_share/client/main.js
+++ 103/image_share/client/main.js
@@ -3,6 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Images = new Mongo.Collection("images");
+
 var img_data = [
   {
     img_src: "IMG_01.JPG",
@@ -30,11 +32,55 @@ var img_data = [
 
 ];
 
-Template.images.helpers({images:img_data});
+//Template.images.helpers({images:img_data});
+
+Template.images.helpers({images:Images.find({}, {sort:{createdOn:-1, rating:-1}})});
 
 Template.images.events({
   'click .js-image': function(event){
     $(event.target).css("width", "80%");
+  },
+
+  'click .js-del-image': function(event){
+    var image_id = this._id;
+    console.log(image_id);
+    //Images.remove({"_id":image_id});
+    $("#"+image_id).hide('slow', function(){
+      Images.remove({"_id":image_id});
+    });
+  },
+
+  'click .js-rate-image': function(event){
+    var rating = $(event.currentTarget).data("userrating");
+    var image_id = this.id;
+
+    Images.update({_id:image_id}, 
+                  {$set: {rating:rating}});
+
+    console.log("image "+image_id+" has "+rating+" stars");
+  },
+
+  'click .js-show-image-form': function(event){
+    $("#image_add_form").modal('show');
+  }
+});
+
+Template.image_add_form.events({
+  'submit .js-add-image': function(event){
+    var img_src, img_alt
+    img_src = event.target.img_src.value;
+    img_alt = event.target.img_alt.value;
+    console.log("src: "+img_src+" alt: "+img_alt);
+    Images.insert(
+      {
+          img_src: img_src,
+          img_alt: img_alt,
+          img_description: "teste lala",
+          createdOn: new Date()
+      }
+      );
+      $("#image_add_form").modal('hide');
+    return false;
   }
 });
 
--- 103/image_share/server/main.js
+++ 103/image_share/server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
+Meteor.startup(function(){
+  Images = new Mongo.Collection("images");
+  console.log("server main.js says: "+Images.find().count());
+
   console.log("I am the server!");
+    if (Images.find().count() == 0) {
+      for (var i=1; i<5; i++) {
+        Images.insert(
+          {
+              img_src: "IMG_0"+i+".JPG",
+              img_alt: "Image number "+i,
+              img_description: "Description of image number "+i
+          }
+          );
+      }
+    }
+
 });
